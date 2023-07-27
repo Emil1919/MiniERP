7def486c21aec0047fbe0059454bc0dcae9cb307
EmilYordanov1919 <yordanov.emo@gmail.com>
2023-07-28 02:40:31 +0300

    added products

---
+++ MiniERP.Services.Data/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using Mini_ERP.Data;
+using MiniERP.Data.Models.CustomerData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniERP.Services.Data
+{
+	public class ProductService: Interfaces.IProductService
+	{
+		private readonly MiniERP_DbContext dbContext;
+        public ProductService(MiniERP_DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task < HashSet<Product>> GetAllProducts()
+		{
+			return await dbContext.Products().ToHashSetAsync();
+			
+		}
+	}
+}
--- MiniERP_Data/MiniERP_DbContext.cs
+++ MiniERP_Data/MiniERP_DbContext.cs
@@ -6,19 +6,19 @@ using MiniERP.Data.Models.OwnerData;
 namespace Mini_ERP.Data
 {
 	public class MiniERP_DbContext : IdentityDbContext
-    {
-        public MiniERP_DbContext(DbContextOptions<MiniERP_DbContext> options)
-            : base(options)
-        {
+	{
+		public MiniERP_DbContext(DbContextOptions<MiniERP_DbContext> options)
+			: base(options)
+		{
 
-        }
-        DbSet<Invoice> Invoices { get; set; } = null!;
-        DbSet<Customer> Customers { get; set; } = null!;
-        DbSet<Order> Orders { get; set; } = null!;
-        DbSet<Product> Products { get; set; } = null!;
-        DbSet<ShipingAdress> Shippings { get; set; } = null!;
-        DbSet<Owner> Owners { get; set; } = null!;
-        
+		}
+		public DbSet<Invoice> Invoices { get; set; } = null!;
+		public DbSet<Customer> Customers { get; set; } = null!;
+		public DbSet<Order> Orders { get; set; } = null!;
+		public DbSet<Product> Products { get; set; } = null!;
+		public DbSet<ShipingAdress> Shippings { get; set; } = null!;
+		public DbSet<Owner> Owners { get; set; } = null!;
 
-    }
+
+	}
 }

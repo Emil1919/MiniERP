a2cabc8c23d692de20ceece01791d063f7f4c907
EmilYordanov1919 <yordanov.emo@gmail.com>
2023-08-12 17:04:53 +0300

    removed bugs

--- MiniERP.Web.ViewModels/ProductViewModel.cs
+++ MiniERP.Web.ViewModels/ProductViewModel.cs
@@ -22,7 +22,9 @@ namespace MiniERP.Web.ViewModels
 		[MinLength(GeneralConstants.NameMinLength)]
 		[Required]
 		public string Description { get; set; }= null!;
+		[Range(0.1, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}$")]
 		public decimal Price { get; set; }
+		[Range(0, int.MaxValue, ErrorMessage = "The quantity {0} must be greater than {1} psc")]
 		public int Quantity { get; set; }
 
 		[MaxLength(GeneralConstants.URL_MaxLenght)]
